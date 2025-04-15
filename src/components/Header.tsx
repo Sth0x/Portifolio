8ef79137c12e82941a9bfa91c6f719bab1839eae
Sth0x <felipe.sts09@gmail.com>
2025-04-15 07:20:14 -0300

    ajustado header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,6 @@ function Header() {
     return (
       <header className="bg-white-500 text-white p-4 shadow-sm">
         <h1 className="bg-clip-text text-transparent inline-block bg-gradient-to-r from-blue-500 via-green-500 to-blue-500  text-2xl font-bold font-mono ml-42">Felipe Santos</h1>
-        <p>
-          Inicio
-        </p>
       </header>
     );
   }
